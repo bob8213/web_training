afe3de936ba8c688273e7bccd0e44e5ab41cfd09
NewBobPlus <rschuhler@hotmail.fr>
2020-03-18 15:47:40 +0100

    Chess: Replaced table with divs

--- chess/chess.php
+++ chess/chess.php
@@ -12,66 +12,64 @@ function createPieces()
 
   //Reverse order for white player
   $whiteRow = [];
-  for ($i=7; $i >= 0; $i--) {
-    $whiteRow[$i] = $blackRow[7-$i];
-  }
+  for ($i=7; $i >= 0; $i--) $whiteRow[$i] = $blackRow[7-$i];
 
   $black = array_merge($blackRow, $row2);
   $white = array_merge($row2, $whiteRow);
 
   //Add color information
-  for ($i=0; $i < 16; $i++) $white[$i] .= ' BLANC';
-  for ($i=0; $i < 16; $i++) $black[$i] .= ' NOIR';
+  for ($i=0; $i < 16; $i++) {
+    $black[$i] .= ' NOIR';
+    $white[$i] .= ' BLANC';
+  }
 
   return [$black, $white];
 }
 
-function createCells($pieces, $cellSize)
+function createGrid($pieces, $cellSize)
 {
-  $table = "<table><tbody>";
+  $grid = "<main>";
   $i = 0;
   $isWhite = true;
 
   for ($x=0; $x < 8; $x++) {
-    $row = "<tr>";
 
     for ($y=0; $y < 8; $y++) {
-
       $text = "";
       if ($i < 16) $text = $pieces[0][$i];
       elseif ($i >= 48) $text = $pieces[1][$i-48];
 
-      //Add cell to row
-      $row .= "<td class='".($isWhite ? "white" : "black")."'>".
+      $grid .= "<div class='".($isWhite ? "white" : "black")."'>".
         $text.
-      "</td>";
+      "</div>";
 
       $i++;
       $isWhite = !$isWhite;
     }
 
     $isWhite = !$isWhite;
-    $row .= "</tr>";
-    $table .= $row;
   }
 
   // Close table and add style
-  $table .= "</tbody></table>";
-  $table .= "
+  $grid .= "
     <style type='text/css'>
       * {
         box-sizing: border-box;
         text-align: center;
       }
 
-      table {
-        border-collapse: collapse;
+      main {
+        display: flex;
+        flex-wrap: wrap;
+        width: ".($cellSize*8).";
       }
 
-      td {
+      .white, .black {
         border: 2px solid black;
-        width: $cellSize;
-        height: $cellSize;
+        min-width: ".$cellSize.";
+        min-height: ".$cellSize.";
+        flex: 0 0 ".(1/8*100)."%;
+        padding: 20px;
       }
 
       .white {
@@ -86,11 +84,11 @@ function createCells($pieces, $cellSize)
     </style>
   ";
 
-  return $table;
+  return $grid."</main>";
 }
 
 $pieces = createPieces();
-$cells = createCells($pieces, 100);
+$cells = createGrid($pieces, 100);
 
 echo $cells;
 
