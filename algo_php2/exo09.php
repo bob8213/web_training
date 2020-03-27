ef14532aee6af830330a94a871fd8bc1bfff63d5
NewBobPlus <rschuhler@hotmail.fr>
2020-03-27 08:51:07 +0100

    Algo_php2/exo9.php radio fixes

--- algo_php2/exo09.php
+++ algo_php2/exo09.php
@@ -11,12 +11,30 @@ function displayRadios($array) {
   $boxes = '';
   foreach ($array as $item) {
     $low = strtolower($item);
-    $boxes .= '<div><input type="radio" id="'.$low.'">';
-    $boxes .= '<label for="'.$low.'">'.$item.'</label></div>';
+    $boxes .= "
+      <div>
+        <input onclick='tick(this)' type='radio' id='$low'>
+        <label for='$low'>$item</label>
+      </div>
+    ";
   }
+
+  $boxes .= "
+    <script type='text/javascript'>
+      function tick(element) {
+        var previous = document.getElementById('lastChecked');
+        if (previous != null) {
+          previous.id = '';
+          previous.checked = false;
+        }
+        element.id = 'lastChecked';
+      }
+    </script>
+  ";
+
   return $boxes;
 }
 
-echo exo_n(9, displayCheckBoxes($array));
+echo exo_n(9, displayRadios($array));
 
 ?>
